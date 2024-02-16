58d7447f3a7c965b12d0ea5b8c270cf986a70dc0
Roman <management.romandj@gmail.com>
2024-02-16 10:02:11 +0100

    componentes recien creados todo vacio, solo la estructura

---
+++ appProyecto/appViajes/src/app/experiencias/experiencia-individual/experiencia-individual.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia-individual',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiencia-individual.component.html',
+  styleUrl: './experiencia-individual.component.css'
+})
+export class ExperienciaIndividualComponent {
+
+}
---
+++ appProyecto/appViajes/src/app/experiencias/experiencia-nueva/experiencia-nueva.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencia-nueva',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiencia-nueva.component.html',
+  styleUrl: './experiencia-nueva.component.css'
+})
+export class ExperienciaNuevaComponent {
+
+}
