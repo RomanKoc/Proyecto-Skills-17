b680281f2e15ec3841463eb3becbf9f322a006de
Roman <management.romandj@gmail.com>
2024-02-18 00:17:22 +0100

    añadidas todas las rutas y pagina de registro que necesita pequeños ajustes de margenes, hover y cambio de icono

--- appProyecto/appViajes/src/app/app.routes.ts
+++ appProyecto/appViajes/src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { ExperienciasComponent } from './experiencias/experiencias.component';
+import { ExperienciaIndividualComponent } from './experiencias/experiencia-individual/experiencia-individual.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { InfoUsuarioComponent } from './info-usuario/info-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: BienvenidaComponent
+    },
+    {
+        path: 'bienvenida',
+        component: BienvenidaComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'registro',
+        component: RegistroComponent
+    },
+    {
+        path: 'info-usuario',
+        component: InfoUsuarioComponent
+    },
+    {
+        path: 'experiencias',
+        component: ExperienciasComponent,
+        children: [
+            {
+                path: ':experiencia-individual/:id',
+                component: ExperienciaIndividualComponent
+            },
+            {
+                path: ':experiencia-nueva', /* Creo que no hay que meter id, revisar */
+                component: ExperienciaIndividualComponent
+            },
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'bienvenida'
+    }
+];
