b7f722eafc190e0d2ce1f7015fb7d9897b111fbb
Roman <management.romandj@gmail.com>
2024-02-20 23:07:22 +0100

    login con sesion solucionado

--- appProyecto/appViajes/src/app/login/login.component.ts
+++ appProyecto/appViajes/src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ApiPruebaService } from '../api-prueba.service';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import * as CryptoJS from 'crypto-js'; // Importar crypto-js
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,29 @@ import * as CryptoJS from 'crypto-js'; // Importar crypto-js
 })
 export class LoginComponent {
 
-  constructor(private usuarioService: ApiPruebaService) { }
+  id = 0;
+  usuarios: any;
+
+  constructor(private usuarioService: ApiPruebaService, private router: Router) {
+    this.usuarioService.retornar()
+      .subscribe((result) => {
+        console.log('result -> ', result);
+        this.usuarios = result
+      });
+  }
 
   formularioRegistro = new FormGroup({
     mail: new FormControl(''),
     password: new FormControl(''),
   });
 
+  /* cifrar el id del usuario para almacenarlo en localsotrage y usarlo de sesion */
+  cifrar(id: number): string {
+    const cifrado = CryptoJS.SHA256(id.toString());
+    return cifrado.toString(CryptoJS.enc.Hex); // Devuelve el cifrado en formato hexadecimal
+  }
+
+  /* el usuario que introduce el pass se debe comparar con el de la bdd y hay que cifrarlo */
   encriptarPasswd() {
     const passwordValue = this.formularioRegistro.value.password;
     if (passwordValue) {
@@ -27,22 +44,33 @@ export class LoginComponent {
       this.formularioRegistro.patchValue({ password: passwordEncriptada });
     }
   }
-  registrarUsuario() {
-    this.encriptarPasswd();
-    const usuario = {
-      mail: this.formularioRegistro.value.mail,
-      password: this.formularioRegistro.value.password
-    };
-    this.usuarioService.insertarUsuario(usuario)
-      .subscribe({
-        next: (response) => {
-          console.log('Usuario insertado correctamente:', response);
-        },
-        error: (error) => {
-          console.error('Error al insertar usuario:', error);
-        }
-      });
+  desencriptarPasswd(passwordEncriptada: string) {
+    return CryptoJS.SHA256(passwordEncriptada).toString();
+  }
 
+  /* buscar usaurio y mirar si tiene la misma pass, se busca por correo */
+  comprobarUsuario() {
+    this.usuarios.forEach((usuario: any) => {
+      if (usuario.mail === this.formularioRegistro.value.mail) {
+        console.log('Usuario encontrado:', usuario);
+        this.encriptarPasswd();
+        const passwordDesencriptada = this.desencriptarPasswd(usuario.password);
+        /* desecriptar a string porque si no no compara y da error!!!!! */
+        if (this.formularioRegistro.value.password == passwordDesencriptada) {
+          console.log('Usuario logeado correctamente');
+          this.id = usuario.id;
+          localStorage.setItem('userId', this.id.toString());
+          this.router.navigate(['/bienvenida']);
+        } else {
+          console.log('Contraseña incorrecta');
+        }
+      }
+    });
+  }
+
+  iniciarSesion() {
+    this.encriptarPasswd();
+    this.comprobarUsuario();
     this.formularioRegistro.reset();
   }
 }
